5386948724deaf39dacc1880f8a6038a793e50e4
lij86932 <lij86932@gmail.com>
2026-05-12 09:17:55 +0700

    penambahan backend #1

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
+import pymysql
 import os
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
+app.secret_key = 'secret123'
 
-mysql = MySQL(app)
+
+# ================= KONEKSI DATABASE =================
+def get_db():
+    return pymysql.connect(
+        host='localhost',
+        user='root',
+        password='',
+        database='db_sipmas',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
 
 # ================= LOGIN =================
 @app.route('/', methods=['GET','POST'])
@@ -15,21 +25,25 @@ def login():
         email = request.form['email']
         password = request.form['password']
 
-        cur = mysql.connection.cursor()
+        db = get_db()
+        cur = db.cursor()
+
         cur.execute("SELECT * FROM users WHERE email=%s", (email,))
         user = cur.fetchone()
 
-        if user and check_password_hash(user[3], password):
+        if user and check_password_hash(user['password'], password):
             session['login'] = True
-            session['id_user'] = user[0]
-            session['role'] = user[5]
+            session['id_user'] = user['id_user']
+            session['role'] = user['role']
 
-            if user[5] == 'petugas':
+            if user['role'] == 'petugas':
                 return redirect(url_for('dashboard_admin'))
             else:
                 return redirect(url_for('dashboard_user'))
         else:
             flash('Email / Password salah')
+
+        db.close()
 
     return render_template('login.html')
 
@@ -40,6 +54,7 @@ def register():
     if request.method == 'POST':
         nama = request.form['nama']
         email = request.form['email']
+        nik = request.form['nik']
         no_hp = request.form['no_hp']
         password = request.form['password']
         confirm = request.form['confirm_password']
@@ -50,23 +65,26 @@ def register():
 
         hash_password = generate_password_hash(password)
 
-        cur = mysql.connection.cursor()
+        db = get_db()
+        cur = db.cursor()
 
-        # CEK EMAIL
+        # cek email
         cur.execute("SELECT * FROM users WHERE email=%s", (email,))
         user = cur.fetchone()
 
         if user:
             flash('Email sudah terdaftar!')
+            db.close()
             return redirect(url_for('register'))
 
-        # INSERT
+        # insert
         cur.execute("""
-            INSERT INTO users(nama,email,no_hp,password)
-            VALUES(%s,%s,%s,%s)
-        """, (nama,email,no_hp,hash_password))
+            INSERT INTO users(nama,email,nik,no_hp,password)
+            VALUES(%s,%s,%s,%s,%s)
+        """, (nama,email,nik,no_hp,hash_password))
 
-        mysql.connection.commit()
+        db.commit()
+        db.close()
 
         flash('Registrasi berhasil!')
         return redirect(url_for('login'))
@@ -80,9 +98,13 @@ def dashboard_user():
     if 'login' not in session:
         return redirect(url_for('login'))
 
-    cur = mysql.connection.cursor()
+    db = get_db()
+    cur = db.cursor()
+
     cur.execute("SELECT * FROM complaint WHERE id_user=%s", (session['id_user'],))
     data = cur.fetchall()
+
+    db.close()
 
     return render_template('dashboard_user.html', data=data)
 
@@ -90,9 +112,13 @@ def dashboard_user():
 # ================= DASHBOARD ADMIN =================
 @app.route('/dashboard_admin')
 def dashboard_admin():
-    cur = mysql.connection.cursor()
+    db = get_db()
+    cur = db.cursor()
+
     cur.execute("SELECT * FROM complaint")
     data = cur.fetchall()
+
+    db.close()
 
     return render_template('dashboard_admin.html', data=data)
 
@@ -100,7 +126,9 @@ def dashboard_admin():
 # ================= TAMBAH PENGADUAN =================
 @app.route('/tambah', methods=['GET','POST'])
 def tambah():
-    cur = mysql.connection.cursor()
+    db = get_db()
+    cur = db.cursor()
+
     cur.execute("SELECT * FROM categories")
     kategori = cur.fetchall()
 
@@ -113,25 +141,34 @@ def tambah():
 
         file = request.files['file']
         filename = file.filename
-        file.save(os.path.join('static/uploads', filename))
+
+        if filename:
+            file.save(os.path.join('static/uploads', filename))
 
         cur.execute("""
             INSERT INTO complaint(id_user, category_id, title, deskripsi, address, priority, attachment)
             VALUES(%s,%s,%s,%s,%s,%s,%s)
         """, (session['id_user'], category_id, title, deskripsi, address, priority, filename))
 
-        mysql.connection.commit()
+        db.commit()
+        db.close()
+
         return redirect(url_for('dashboard_user'))
 
+    db.close()
     return render_template('tambah_pengaduan.html', kategori=kategori)
 
 
 # ================= UPDATE STATUS =================
 @app.route('/update/<id>')
 def update(id):
-    cur = mysql.connection.cursor()
+    db = get_db()
+    cur = db.cursor()
+
     cur.execute("UPDATE complaint SET status='selesai' WHERE complaint_id=%s", (id,))
-    mysql.connection.commit()
+    db.commit()
+    db.close()
+
     return redirect(url_for('dashboard_admin'))
 
 
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    MYSQL_HOST='localhost'
-    MYSQL_USER='root'
-    MYSQL_PASSWORD=''
-    MYSQL_DB='db_sipmas'
-    SECRET_KEY='secret'
+    SECRET_KEY = 'secret123'
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'db'
